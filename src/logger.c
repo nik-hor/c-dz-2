bbab29a1bf1096fa1c7186a7c59fd9f2ec2c4c0b
nik-hor <66645195+nik-hor@users.noreply.github.com>
2020-06-09 09:51:21 +0300

    error fix

--- src/logger.c
+++ src/logger.c
@@ -22,8 +22,8 @@ void log_action(const char *fmt, ...) {
 
     gettimeofday(&tv, NULL);
     int ms = tv.tv_usec / 1000;
-    // long long int sec = tv.tv_sec;
-    struct tm *lt = localtime(&tv.tv_sec);
+    long long int sec = tv.tv_sec;
+    struct tm *lt = localtime(&sec);
     
     if (L.fp) {
         va_list args;
@@ -31,7 +31,7 @@ void log_action(const char *fmt, ...) {
         buf[strftime(buf, sizeof(buf), "%d-%m-%Y %H:%M:%S", lt)] = 0; // инициализация буфера с датой и временем
         fprintf(L.fp, "%s.%03d: ", buf, ms); // записываем буфер
         va_start(args, fmt);
-        vfprintf(L.fp, fmt, args); // системная функция, как printf только в файл и с использованием переменного числа аргументов
+        vfprintf(L.fp, fmt, args); // системная функция, как fprintf только с использованием переменного числа аргументов
         va_end(args);
         fprintf(L.fp, "\n");
         fflush(L.fp);
